5ea46531eb3091c0247a3d72d65c1beac2e96c16
galibshakib66 <hudaisniper@gmail.com>
2024-06-08 15:12:11 +0600

    production url

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseUrl = "http://localhost:5000";
+export const baseUrl = "https://eats-exchange-server.vercel.app";
 
 const axiosPublic = axios.create({
     baseURL: baseUrl,
